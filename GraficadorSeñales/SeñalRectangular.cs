3ea3534377b6c48e924fde20c1178a8d06ee48c7
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2018-11-05 09:22:39 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalRectangular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalRectangular : Señal
+    {
+        public SeñalRectangular()
+        {
+            Muestras = new List<Muestra>();
+            AmplitudMaxima = 0.0;
+        }
+
+        override public double evaluar(double tiempo)
+        {
+            double resultado;
+            resultado = Math.Abs(tiempo);
+            if (Math.Abs(tiempo) > 0.5)
+            {
+                resultado = 0;
+            }
+            else if (Math.Abs(tiempo) == 0.5)
+            {
+                resultado = 1 / 2;
+            }
+            else if (Math.Abs(tiempo) < 0.5)
+            {
+                resultado = 1;
+            }
+
+              return resultado;
+        }
+    }
+}
